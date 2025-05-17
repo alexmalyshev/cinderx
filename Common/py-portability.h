5677fb73224cfff19a0b52e0e2577827e39f0e0c
Alex Malyshev <lex.malyshev@gmail.com>
2025-05-17 03:27:11 -0400

    Add more polyfills to py-portability.h

--- Common/py-portability.h
+++ Common/py-portability.h
@@ -9,6 +9,7 @@
 
 #if PY_VERSION_HEX >= 0x030E0000
 #include "internal/pycore_genobject.h"
+#include "internal/pycore_frame.h"
 #endif
 
 #if PY_VERSION_HEX < 0x030C0000
@@ -34,7 +35,6 @@
 #define Py_IsFinalizing _Py_IsFinalizing
 
 #define _PyFrame_GetCode(F) ((F)->f_code)
-
 inline int PyList_Extend(PyObject* list, PyObject* iterable) {
   if (!PyList_Check(list)) {
     PyErr_BadInternalCall();
@@ -47,6 +47,11 @@ inline int PyList_Extend(PyObject* list, PyObject* iterable) {
 static inline int PyTime_MonotonicRaw(PyTime_t* result) {
   *result = _PyTime_GetMonotonicClock();
   return 0;
+}
+
+inline void PyErr_FormatUnraisable(const char* fmt, ...) {
+  // This isn't correct, but that's okay for now.
+  return _PyErr_WriteUnraisableMsg(fmt, NULL);
 }
 
 #endif
